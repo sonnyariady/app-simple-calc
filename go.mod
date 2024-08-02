b2094d72f3091e05bfd6343fa500d158d39f3584
Sonny Ariady <sonnyariady@gmail.com>
2024-08-02 10:11:38 +0700

    update modul yg dipanggil

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/sonnyariady/app-simple-calc
 
 go 1.22.5
 
-require github.com/sonnyariady/go-simple-calc v1.0.0 // indirect
+require github.com/sonnyariady/go-simple-calc v1.5.0
--- main.go
+++ main.go
@@ -7,10 +7,12 @@ import (
 )
 
 func main() {
-	a := float32(12)
-	b := float32(4)
+	a := float64(3)
+	b := float64(4)
 	fmt.Println(sclc.Jumlah(a, b))
 	fmt.Println(sclc.Kali(a, b))
 	fmt.Println(sclc.Bagi(a, b))
 	fmt.Println(sclc.Kurang(a, b))
+	fmt.Println(sclc.Pitagoras(a, b))
+	fmt.Println(sclc.JumlahKuadrat(a, b))
 }
